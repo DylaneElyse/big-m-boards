a69322ba051fbdb05cdae6ddcf7673b21bbd79f7
Dylane Cunningham <155758209+DylaneElyse@users.noreply.github.com>
2025-10-08 19:50:01 -0600

    Add favicon and manifest links to layouts
    Updated both site and admin layouts to include favicon, apple-touch-icon, PNG, SVG, and manifest links in the <Head> section for improved PWA and icon support. Updated icon and manifest assets, and added a new horizontal logo image.

--- app/(site)/layout.js
+++ app/(site)/layout.js
@@ -1,14 +1,26 @@
+
 import SiteHeader from '@/components/SiteHeader';
 import SiteFooter from '@/components/SiteFooter';
+import Head from 'next/head';
 
 export default function SiteLayout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen bg-gray-50">
-      <SiteHeader />
-      <div className='flex-grow'>
-        {children}
+    <>
+      <Head>
+        <link rel="icon" href="/app/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/app/apple-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/app/icon1.png" />
+        <link rel="icon" type="image/svg+xml" href="/app/icon0.svg" />
+        <link rel="manifest" href="/app/manifest.json" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <div className="flex flex-col min-h-screen bg-gray-50">
+        <SiteHeader />
+        <div className='flex-grow'>
+          {children}
+        </div>
+        <SiteFooter />
       </div>
-      <SiteFooter />
-    </div>
+    </>
   );
 }
